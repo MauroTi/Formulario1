8455ba9e75261312b8282b58abc87fde3b04d706
Mauro <mauro.guterres@alcidesmaya.edu.br>
2025-04-16 13:26:37 -0300

    Alterações sugeridas visual studioAdicionar arquivos de projeto.

---
+++ DAO/ConexaoDao.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Formulario1.DAO
+{
+    public class ConexaoDAO
+    {
+        private MySqlConnection conexao;
+
+        private string stringConexao = "server=localhost;database=erpalfa;uid=root;pwd=;";
+
+        public MySqlConnection Conectar()
+        {
+            try
+            {
+                conexao = new MySqlConnection(stringConexao);
+                conexao.Open();
+                Console.WriteLine("Conexão realizada com sucesso!");
+                return conexao;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+                return null;
+            }
+        }
+
+        public void Desconectar()
+        {
+            if (conexao != null)
+            {
+                conexao.Close();
+                Console.WriteLine("Conexão encerrada.");
+            }
+        }
+    }
+}
